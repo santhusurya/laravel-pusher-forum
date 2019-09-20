d8526a00e5c2540ff2b334ebe4ef397cb0e0cc83
Santhosh Suryavanshi <santhusurya@gmail.com>
2019-09-20 19:30:51 +0530

    Like & Dislike System for Reply

--- routes/api.php
+++ routes/api.php
@@ -24,3 +24,6 @@ Route::apiResource('/question', 'QuestionController');
 Route::apiResource('/category', 'CategoryController');
 
 Route::apiResource('/question/{question}/reply', 'ReplyController');
+
+Route::post('/like/{reply}','LikeController@likeit');
+Route::delete('/like/{reply}', 'LikeController@unlikeit');
