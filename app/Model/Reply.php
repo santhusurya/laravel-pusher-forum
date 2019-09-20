49f1bd0299034086c7d5a61148bdee0e32922e3e
Santhosh Suryavanshi <santhusurya@gmail.com>
2019-09-20 19:14:01 +0530

    CRUD for Reply. Added Resource for Category & Reply Models

--- app/Model/Reply.php
+++ app/Model/Reply.php
@@ -7,11 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
+
+    protected $guarded = [];
+
     public function question()
     {
         return $this->belongsTo(Question::class);
     }
-    
+
     public function user()
     {
         return $this->belongsTo(User::class);
